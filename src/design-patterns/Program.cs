68c494bb960ae53fdd9933b2c66f35a0f0994a80
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-12-20 09:21:10 -0300

    Implementation

--- src/design-patterns/Program.cs
+++ src/design-patterns/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using design_patterns.Patterns.StrategyPattern.Abstract;
+using design_patterns.Patterns.StrategyPattern.Concrete;
+
+public class Program
+{    
+    static void Main() {
+        Character character = new King();
+        character.UseWeapon();
+        
+        character = new Queen();
+        character.UseWeapon();
+
+        character = new Troll();
+        character.UseWeapon();
+
+        character.SetWeapon(new BowAndArrowBehavior());
+        character.UseWeapon();
+
+        character = new Knight();
+        character.UseWeapon();
+    }
+}
