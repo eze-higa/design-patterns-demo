32be4e0e5d9e56f962659c1a3e05d223925aa975
eze-higa <53649465+eze-higa@users.noreply.github.com>
2022-01-03 19:37:24 -0300

    Create StrategyPatternImplementation.cs

---
+++ src/design-patterns/Patterns/StrategyPattern/StrategyPatternImplementation.cs
@@ -0,0 +1,30 @@
+﻿using design_patterns.Patterns.StrategyPattern.Abstract;
+using design_patterns.Patterns.StrategyPattern.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Patterns.StrategyPattern
+{
+    public static class StrategyPatternImplementation
+    {
+        public static void Run() {
+            Character character = new King();
+            character.UseWeapon();
+
+            character = new Queen();
+            character.UseWeapon();
+
+            character = new Troll();
+            character.UseWeapon();
+
+            character.SetWeapon(new BowAndArrowBehavior());
+            character.UseWeapon();
+
+            character = new Knight();
+            character.UseWeapon();
+        }
+    }
+}
