b60bd491e30af0766cf493fef8497ea27ff5b51c
eze-higa <53649465+eze-higa@users.noreply.github.com>
2022-01-03 19:37:35 -0300

    Add Singleton Pattern

---
+++ src/design-patterns/Patterns/SingletonPattern/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Patterns.SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton _singleton = new Singleton();
+        public string Message { get; set; }
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            return _singleton;
+        }
+
+    }
+}
---
+++ src/design-patterns/Patterns/SingletonPattern/SingletonPatternImplementation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Patterns.SingletonPattern
+{
+    public static class SingletonPatternImplementation
+    {
+        public static void Run()
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Singleton secondSingleton = Singleton.GetInstance();
+
+            singleton.Message = "Test";
+            Console.WriteLine($"First singleton {singleton.Message}");
+            Console.WriteLine($"Second singleton {secondSingleton.Message}");
+
+            secondSingleton.Message = "new test";
+            Console.WriteLine($"First singleton {singleton.Message}");
+            Console.WriteLine($"Second singleton {secondSingleton.Message}");
+        }
+    }
+}
